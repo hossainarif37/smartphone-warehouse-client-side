b717b6571c3e67b80b2f44b57f27686e1b5c97b6
hossainarif37 <hossainarif4937@gmail.com>
2022-05-16 21:30:43 +0600

    again updated some code of inventory item

--- src/Components/InventoryItem/InventoryItem.js
+++ src/Components/InventoryItem/InventoryItem.js
@@ -9,7 +9,7 @@ const InventoryItem = ({ product }) => {
                 <div className="card">
                     <img src={img} className="card-img-top w-100" alt="..." />
                     <div className="card-body">
-                        <h3 className="card-title">{name}</h3>
+                        <h3 className="card-title">{name.slice(0, 18)}</h3>
                         <h5>Supplier: {supplier}</h5>
                         <h5>Price: {price}</h5>
                         <p className="card-text">{description.slice(0, 118)}...</p>
